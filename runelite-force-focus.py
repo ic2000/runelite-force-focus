68bd5f0f135e1e1224a34b1e1bdcda4eebe1ef9e
ic2000 <ic2000@pop-os.localdomain>
2022-09-16 22:25:28 +0100

    follow notifs directly to support multiple client windows

--- runelite-force-focus.py
+++ runelite-force-focus.py
@@ -6,39 +6,33 @@ from subprocess import PIPE, STDOUT, Popen, check_output, run
 import shlex
 
 
-def get_window_title():
-    window_titles = check_output(("wmctrl", "-l")).decode().rstrip()
-    r = re.compile("^[^ ]+  [^ ]+ [^ ]+ RuneLite - \w+$")
-    window_titles = window_titles.split("\n")
-    window_title = list(filter(r.match, window_titles))[0]
-    return window_title[window_title.find("RuneLite") :]
-
-
 def main():
     journalctl = Popen(
-        "journalctl -f --since '0 sec ago'", stdout=PIPE, stderr=STDOUT, shell=True
+        'dbus-monitor "interface=\'org.freedesktop.Notifications\'" | grep --line-buffered  "member=Notify\|string"',
+        stdout=PIPE,
+        stderr=STDOUT,
+        shell=True,
     )
 
     for line in journalctl.stdout:
         line = line.decode().rstrip()
+        runelite_notif = re.search(r"\"(RuneLite - \w+)\"", line)
 
-        if not ("runelite" in line and "notify-send" in line):
-            continue
+        if runelite_notif:
+            window_title = runelite_notif.group(1)
 
-        window_title = get_window_title()
-
-        active_window_title = (
-            check_output(("xdotool", "getwindowfocus", "getwindowname"))
-            .decode()
-            .rstrip()
-        )
-
-        if window_title != active_window_title:
-            run(
-                shlex.split(
-                    f"xdotool search --any --name '{window_title}' windowactivate"
-                )
+            active_window_title = (
+                check_output(("xdotool", "getwindowfocus", "getwindowname"))
+                .decode()
+                .rstrip()
             )
+
+            if window_title != active_window_title:
+                run(
+                    shlex.split(
+                        f"xdotool search --any --name '{window_title}' windowactivate"
+                    )
+                )
 
     return 0
 
