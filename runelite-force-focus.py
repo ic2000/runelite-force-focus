9d5a854dffa3281e37cd49234368ca67c1bd8e7e
ic2000 <ic2000@pop-os.localdomain>
2022-09-16 21:58:00 +0100

    remove time checking

--- runelite-force-focus.py
+++ runelite-force-focus.py
@@ -4,11 +4,10 @@ import re
 import sys
 from subprocess import PIPE, STDOUT, Popen, check_output, run
 import shlex
-from datetime import datetime
 
 
 def get_window_title():
-    window_titles = check_output(["wmctrl", "-l"]).decode().rstrip()
+    window_titles = check_output(("wmctrl", "-l")).decode().rstrip()
     r = re.compile("^[^ ]+  [^ ]+ [^ ]+ RuneLite - \w+$")
     window_titles = window_titles.split("\n")
     window_title = list(filter(r.match, window_titles))[0]
@@ -16,31 +15,30 @@ def get_window_title():
 
 
 def main():
-    journalctl = Popen("journalctl -f", stdout=PIPE, stderr=STDOUT, shell=True)
+    journalctl = Popen(
+        "journalctl -f --since '0 sec ago'", stdout=PIPE, stderr=STDOUT, shell=True
+    )
 
     for line in journalctl.stdout:
         line = line.decode().rstrip()
 
-        if "runelite" in line and "notify-send" in line:
-            time = line.split(" ")[2]
+        if not ("runelite" in line and "notify-send" in line):
+            continue
 
-            if time != datetime.now().strftime("%H:%M:%S"):
-                continue
+        window_title = get_window_title()
 
-            active_window_title = (
-                check_output(["xdotool", "getwindowfocus", "getwindowname"])
-                .decode()
-                .rstrip()
-            )
+        active_window_title = (
+            check_output(("xdotool", "getwindowfocus", "getwindowname"))
+            .decode()
+            .rstrip()
+        )
 
-            window_title = get_window_title()
-
-            if window_title != active_window_title:
-                run(
-                    shlex.split(
-                        f"xdotool search --any --name '{window_title}' windowactivate"
-                    )
+        if window_title != active_window_title:
+            run(
+                shlex.split(
+                    f"xdotool search --any --name '{window_title}' windowactivate"
                 )
+            )
 
     return 0
 
