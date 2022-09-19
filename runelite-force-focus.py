b94f4c9ada506253e282082adafc209b91bcf465
ic2000 <ic2000@pop-os.localdomain>
2022-09-19 19:21:09 +0100

    remove active window checking

--- runelite-force-focus.py
+++ runelite-force-focus.py
@@ -7,35 +7,25 @@ import shlex
 
 
 def main():
-    journalctl = Popen(
+    notifs = Popen(
         'dbus-monitor "interface=\'org.freedesktop.Notifications\'" | grep --line-buffered  "member=Notify\|string"',
         stdout=PIPE,
         stderr=STDOUT,
         shell=True,
     )
 
-    for line in journalctl.stdout:
+    for line in notifs.stdout:
         line = line.decode().rstrip()
         runelite_notif = re.search(r"\"(RuneLite - \w+)\"", line)
 
         if runelite_notif:
             window_title = runelite_notif.group(1)
 
-            try:
-                active_window_title = (
-                    check_output(("xdotool", "getwindowfocus", "getwindowname"))
-                    .decode()
-                    .rstrip()
+            run(
+                shlex.split(
+                    f"xdotool search --any --name '{window_title}' windowactivate"
                 )
-            except CalledProcessError: # no active window
-                active_window_title = None
-
-            if window_title != active_window_title:
-                run(
-                    shlex.split(
-                        f"xdotool search --any --name '{window_title}' windowactivate"
-                    )
-                )
+            )
 
     return 0
 
